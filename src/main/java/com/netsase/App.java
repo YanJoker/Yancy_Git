2606c6ce292c240e0a0a3b1c32ff10975729f243
Yancy <1152935152@qq.com>
2019-03-29 10:36:04 +0800

    update:创建新项目

---
+++ src/main/java/com/netsase/App.java
@@ -0,0 +1,13 @@
+package com.netsase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
